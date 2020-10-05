bf72c117c8373681c493bfa05f70562783770626
Alex <57219372+h3llfir3@users.noreply.github.com>
2020-10-05 19:28:16 +0200

    Second commit

--- Prueba/Prueba/Program.cs
+++ Prueba/Prueba/Program.cs
@@ -6,7 +6,9 @@ namespace Prueba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! :)");
+
+            Console.WriteLine("Goodbye World");
         }
     }
 }
